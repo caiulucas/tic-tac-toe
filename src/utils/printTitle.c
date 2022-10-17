a91ab2d4fdf7fba1e2e5421e3f71f1ca4be0fb56
Caio Lucas <lienscarlet@gmail.com>
2022-10-17 17:04:04 -0300

    fix: change printFile to printTitle

---
+++ src/utils/printTitle.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+void printTitle() {
+  printf("\n");
+  printf("\t████████╗██╗ █████╗   ████████╗ █████╗  █████╗   ████████╗ █████╗ ███████╗\n");
+  printf("\t╚══██╔══╝██║██╔══██╗  ╚══██╔══╝██╔══██╗██╔══██╗  ╚══██╔══╝██╔══██╗██╔════╝\n");
+  printf("\t   ██║   ██║██║  ╚═╝     ██║   ███████║██║  ╚═╝     ██║   ██║  ██║█████╗\n");
+  printf("\t   ██║   ██║██║  ██╗     ██║   ██╔══██║██║  ██╗     ██║   ██║  ██║██╔══╝\n");
+  printf("\t   ██║   ██║╚█████╔╝     ██║   ██║  ██║╚█████╔╝     ██║   ╚█████╔╝███████╗\n");
+  printf("\t   ╚═╝   ╚═╝ ╚════╝      ╚═╝   ╚═╝  ╚═╝ ╚════╝      ╚═╝    ╚════╝ ╚══════╝\n");
+  printf("\n\n\n\tBem vindo ao Jogo da Velha\n\n");
+  printf("\t0. Sair do Jogo\n\t1. Começar um novo jogo\n\t2. Continuar um jogo salvo\n\t3. Continuar o jogo atual\n\t4. Exibir o ranking\n");
+}
